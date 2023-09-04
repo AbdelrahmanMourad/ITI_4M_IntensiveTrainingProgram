df915db9474aff517f364d844a0276527027c99d
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-09-04 14:35:41 +0300

    Session 7 C

---
+++ Assignments/00. C Assignments/Week 2/Session 7/lab4.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//	--------------
+//	Pre-Processor:
+//	--------------
+Session -1-	Lab	-1-	
+/**/
+//
+
+
+void main(void)
+{
+	
+}
---
+++ Assignments/00. C Assignments/Week 2/Session 7/lab5.c
@@ -0,0 +1,11 @@
+#include <iostream>
+
+//	Session -1-	Lab	-1-	
+/**/
+//
+
+
+void main(void)
+{
+	
+}
