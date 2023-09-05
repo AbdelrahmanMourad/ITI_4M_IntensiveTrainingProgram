9be883d1afd3caefbb18fb6eece630c6927548fd
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-09-05 23:33:38 +0300

    Session 9 Data Structure

---
+++ Assignments/00. C Assignments/Week 2/Session 9/main.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+ #include <stdlib.h>		// maalloc,free
+ #include "STD_TYPES.h"		// using u8/u16
+ #include "LinkedList.h"
+ 
+ void main (void)
+ {
+	 // LinkedList_voidInserNodeAtTail(3);
+	 u8 x = 5;
+	 printf("%d",x);
+
+	}
+ 
