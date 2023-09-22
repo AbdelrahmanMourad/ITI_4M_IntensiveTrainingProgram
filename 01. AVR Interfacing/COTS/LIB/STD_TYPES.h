bb858bc607dd67358f776b16ca1b843cec084cfa
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-09-22 07:45:10 +0300

    Minor Updates To LIB.

--- 01. AVR Interfacing/COTS/LIB/STD_TYPES.h
+++ 01. AVR Interfacing/COTS/LIB/STD_TYPES.h
@@ -12,26 +12,36 @@
 */
 
 
-/*Header Guard File*/
+/* Header Guard File */
 #ifndef     STD_TYPES_H
 #define     STD_TYPES_H
 
-typedef unsigned char 	    	u8;
-typedef unsigned short		    u16;
-typedef unsigned int			u32;
+/* Our DataTypes */
+typedef unsigned char   u8;
+typedef unsigned short  u16;
+typedef unsigned int    u32;
 
-typedef signed char 		    s8;
-typedef signed short		    s16;
-typedef signed int			    s32;
+typedef signed char     s8;
+typedef signed short    s16;
+typedef signed int      s32;
 
+typedef  float 			f32;
+typedef  double         f64;
 
-typedef  float 					f32;
-typedef  double                 f64;
+/* Other Common DataTypes */
+typedef unsigned char   uint8;
+typedef unsigned short	uint16;
+typedef unsigned int	uint32;
 
-#define  TRUE                   1
-#define  FALSE                  0
+typedef signed char 	sint8;
+typedef signed short	sint16;
+typedef signed int		sint32;
 
-#define NULL                    (void *)0
+/* Expressions */
+#define TRUE    1
+#define FALSE   0
+
+#define NULL    (void *)0
 
 #endif  /*STD_TYPES_H*/
 
