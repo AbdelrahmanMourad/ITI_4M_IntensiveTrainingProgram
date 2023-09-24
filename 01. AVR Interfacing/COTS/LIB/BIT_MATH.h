78e2200cc48487f10eddd3bb9b6231bdf8d8da46
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-09-24 10:47:32 +0300

    Update

--- 01. AVR Interfacing/COTS/LIB/BIT_MATH.h
+++ 01. AVR Interfacing/COTS/LIB/BIT_MATH.h
@@ -15,16 +15,16 @@
 #define BIT_MATH_H
 
 /******** Functions Like Macro To Manipulate Over Specific Bit ************/
-#define     SET_BIT(REG_NAME, BIT_NUM)                  (REG_NAME |= (1 << BIT_NUM))
-#define     CLR_BIT(REG_NAME, BIT_NUM)                  (REG_NAME &= (~(1 << BIT_NUM)))
-#define     GET_BIT(REG_NAME, BIT_NUM)                  ((REG_NAME >> BIT_NUM) & 0x01)
-#define     TOG_BIT(REG_NAME, BIT_NUM)                  (REG_NAME ^= (1 << BIT_NUM))
+#define SET_BIT(REG_NAME, BIT_NUM)                  (REG_NAME |= (1 << BIT_NUM))
+#define CLR_BIT(REG_NAME, BIT_NUM)                  (REG_NAME &= (~(1 << BIT_NUM)))
+#define GET_BIT(REG_NAME, BIT_NUM)                  ((REG_NAME >> BIT_NUM) & 0x01)
+#define TOG_BIT(REG_NAME, BIT_NUM)                  (REG_NAME ^= (1 << BIT_NUM))
 
 /******** Functions Like Macro To Manipulate Over Specific Register *******/
-#define     SET_REG_SPECIFIC_VALUE(REG_NAME, VALUE)     (REG_NAME = VALUE)
-#define     SET_REG(REG_NAME)                           (REG_NAME = 0xFF)
-#define     GET_REG(REG_NAME)                           (REG_NAME)
-#define     CLR_REG(REG_NAME)                           (REG_NAME = 0x00)
+#define SET_REG_SPECIFIC_VALUE(REG_NAME, VALUE)     (REG_NAME = VALUE)
+#define SET_REG(REG_NAME)                           (REG_NAME = 0xFF)
+#define GET_REG(REG_NAME)                           (REG_NAME)
+#define CLR_REG(REG_NAME)                           (REG_NAME = 0x00)
 
 
 
