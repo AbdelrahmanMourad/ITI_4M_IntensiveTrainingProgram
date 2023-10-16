e212962acd9d81149c0cf36f60c4afbc75f5a787
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-10-16 03:43:14 +0300

    KPD Driver Header Log File & Header Guard File

---
+++ 01. AVR Interfacing/COTS/HAL/KPD/KPD_private.h
@@ -0,0 +1,22 @@
+/*_____________________________________________________________________________
+ * Author:			Abdelrahman Mourad
+ * Creation Date:	16 Ocr, 2023
+ * Version:			v1.0
+ * Compiler:		GNU GCC
+ * Controller:		ATmega32    (8-Bit Architecture)
+ * Processor:		AVR         (8-Bit Architecture)
+ * Layer:			HAL
+ * Driver:          KPD         (KeyPad 4x4)
+ *  --------------------------------------------------------------------------
+ * Version	  Date				Author					Description
+ * v1.0		  16 Oct, 2023		Abdelrahman Mourad		Initial Creation
+ * ____________________________________________________________________________
+ */
+
+/*  ----------------------------------------------
+                Header Guard File:
+    ---------------------------------------------- */
+#ifndef MCAL_KPD_KPD_PRIVATE_H_
+#define MCAL_KPD_KPD_PRIVATE_H_
+
+#endif /*MCAL_KPD_KPDPRIVATE__H_*/
---
+++ 01. AVR Interfacing/COTS/HAL/KPD/KPD_program.c
@@ -0,0 +1,14 @@
+/*_____________________________________________________________________________
+ * Author:			Abdelrahman Mourad
+ * Creation Date:	16 Ocr, 2023
+ * Version:			v1.0
+ * Compiler:		GNU GCC
+ * Controller:		ATmega32    (8-Bit Architecture)
+ * Processor:		AVR         (8-Bit Architecture)
+ * Layer:			HAL
+ * Driver:          KPD         (KeyPad 4x4)
+ *  --------------------------------------------------------------------------
+ * Version	  Date				Author					Description
+ * v1.0		  16 Oct, 2023		Abdelrahman Mourad		Initial Creation
+ * ____________________________________________________________________________
+ */
