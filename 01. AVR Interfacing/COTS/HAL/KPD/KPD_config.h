ecda23eec7be755445f3d3c08828c438adc4b39c
Abdelrahman Mourad <abdelrahmanmourad.am@gmail.com>
2023-10-16 04:06:33 +0300

    KPD Driver Making Some Configurations As Macros.

--- 01. AVR Interfacing/COTS/HAL/KPD/KPD_config.h
+++ 01. AVR Interfacing/COTS/HAL/KPD/KPD_config.h
@@ -19,4 +19,35 @@
 #ifndef MCAL_KPD_KPD_CONFIG_H_
 #define MCAL_KPD_KPD_CONFIG_H_
 
+/*  ----------------------------------------------
+                Configurations
+    ---------------------------------------------- */
+// Ports
+#define KPD_ROWS_PORT       DIO_PORTD
+#define KPD_COLUMNS_PORT    DIO_PORTD
+// Rows Pins.   
+#define KPD_ROW0_PIN        DIO_PIN0
+#define KPD_ROW1_PIN        DIO_PIN1
+#define KPD_ROW2_PIN        DIO_PIN2
+#define KPD_ROW3_PIN        DIO_PIN3
+// Columns Pins.    
+#define KPD_COLUMN0_PIN     DIO_PIN4
+#define KPD_COLUMN1_PIN     DIO_PIN5
+#define KPD_COLUMN2_PIN     DIO_PIN6
+#define KPD_COLUMN3_PIN     DIO_PIN7
+
+// Set Value To Compare With.
+#define KPD_NOT_PRESSED_VAL 255
+
+// Number of Columns and Rows.
+#define KPD_ROWS_NUM        4
+#define KPD_COLUMN_NUM      4
+
+// KeyPad LayOut.
+#define KPD_ARR_VAL_LAYOUT  { {'1', '2', '3', 'A'}\
+                             ,{'4', '5', '6', 'B'}\
+                             ,{'7', '8', '9', 'C'}\
+                             ,{'*', '0', '#', 'D'} }
+
+
 #endif /*MCAL_KPD_KPDCONFIG__H_*/
